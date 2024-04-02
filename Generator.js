0a032a4670573159d1304453aef4556e013a76ca
Niko-Drossos <8sHe682GSYhaas@protonmail.com>
2024-04-02 15:20:45 -0700

    Needs fixing, wont download appon completion

--- Generator.js
+++ Generator.js
@@ -1,25 +1,51 @@
-const inquirer = require('inquirer')
-const qr = require('qr-image')
-const fs = require('fs')
+const inquirer = require('inquirer');
+const qr = require('qr-image');
+const fs = require('fs');
+const path = require('path')
+const os = require('os');
+const { exec } = require('child_process');
+
+const downloadsDirectory = path.join(os.homedir(), 'Downloads');
 
 inquirer.prompt([
-  {
-    message: "Type in the data of the information you want QR'ed",
-    name: "URL"
-  }
+    {
+        message: "Type in the data of the information you want QR'ed",
+        name: "URL"
+    },
+    {
+        message: "Enter the name of the output file (without extension)",
+        name: "fileName"
+    }
 ]).then(answers => {
-  const url = answers.URL
-  var qr_svg = qr.image(url)
-  qr_svg.pipe(fs.createWriteStream('qr_img.png'))
+    const { URL, fileName } = answers;
+    const qr_svg = qr.imageSync(URL, { type: 'png' }); // Generate QR code as PNG buffer
 
-  fs.writeFile("URL.txt", url, err => {
-    if (err) throw err
-    console.log("The file has been saved!")
-  })
+    const qrDataURL = 'data:image/png;base64,' + qr_svg.toString('base64'); // Convert PNG buffer to data URI
+
+    // Run curl command to download the QR code image
+    const command = `curl --output "${path.join(downloadsDirectory, fileName)}.png" ${qrDataURL}`;
+
+    console.log('Executing command:', command);
+    
+    exec(command, (error, stdout, stderr) => {
+        if (error) {
+            console.error('Error:', error);
+            return;
+        }
+        console.log('QR code downloaded successfully!');
+        console.log('stdout:', stdout);
+        console.error('stderr:', stderr);
+    });
+
+    // Optionally, you can save the URL to a text file
+    fs.writeFile("URL.txt", URL, err => {
+        if (err) throw err;
+        console.log("The file has been saved!");
+    });
 }).catch(error => {
-  if (error.isTtyError) {
-    // Prompt could'nt be rendered in the current environment
-  } else {
-    // Something else went wrong
-  }
-})
+    if (error.isTtyError) {
+        // Prompt couldn't be rendered in the current environment
+    } else {
+        // Something else went wrong
+    }
+});
